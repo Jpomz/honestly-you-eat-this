69196b9402b240d1ac2d4a47e73f00a4cbb910eb
Jpomz <jfpomeranz@gmail.com>
2017-12-31 11:54:35 +1300

    begin script for inferring trophic interactions using gravel trait-matching

---
+++ gravel trait matching inference.R
@@ -0,0 +1,18 @@
+# Trait-matching
+# 31 Dec 2017
+# Justin Pomeranz
+#jfpomeranz@gmail.com
+
+# paramterize Niche model using method presented in Gravel et al. 2013 Methods in Eco Evo. Inferring food web structure from predator-prey body size relationships
+
+library(tidyverse)
+
+# gravel functions
+# from Gravel et al. 2013 supplementary information
+source("gravel_functions.R")
+
+# data ####
+# invertebrate biomass and abundance
+invert <- readRDS("estimated invert bodymass.RDS")
+# fish biomass and abundance
+fish <- readRDS("estimated fish bodymass and abundance.RDS")
