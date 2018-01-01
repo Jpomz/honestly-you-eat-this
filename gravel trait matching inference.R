be4c4b3b6ee442fb9410ff3f6bb3b2775da459b3
Jpomz <jfpomeranz@gmail.com>
2018-01-01 14:56:32 +1300

    ammedn tss() t use base functions and only return TSS measure

--- gravel trait matching inference.R
+++ gravel trait matching inference.R
@@ -31,26 +31,13 @@ tss <- function (observed, inferred){
   # -1 = false positive (b); predicted but not observed
   # 1 = false negative (c); observed but not predicted
   # 0 = true negative (d); not predicted, not observed
-  tss.vars <- data.frame(
-    a = length(prediction[prediction==2]),
-    b = length(prediction[prediction==-1]), 
-    c = length(prediction[prediction==1]), 
-    d = length(prediction[prediction==0]),
-    S = ncol(prediction)
-  )
+    a = length(prediction[prediction==2])
+    b = length(prediction[prediction==-1]) 
+    c = length(prediction[prediction==1]) 
+    d = length(prediction[prediction==0])
   # calculate TSS
   # TSS = (a*d - b*c)/((a+c)*(b+d))
-  # also decompose TSS to see proportion of positive and negative predictions
-  # abar = proportion of links correctly predicted
-  # dbar = proportion of links correctly not predicted
-  # bc = proportion of links incorrectly predicted
-  tss <- tss.vars %>%
-    mutate(tss =(a*d - b*c)/((a+c)*(b+d)),
-           abar = a/S**2,
-           dbar = d / S**2,
-           bbar = b / S**2,
-           cbar = c / S**2,
-           bc = (b + c) / S**2)
+  tss = (a*d - b*c)/((a+c)*(b+d))
   tss
 }
 
