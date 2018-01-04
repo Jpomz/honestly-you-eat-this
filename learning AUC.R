6d8176504f6138dae6f60f80b2702ca698669581
Jpomz <jfpomeranz@gmail.com>
2018-01-05 12:18:35 +1300

    comment out old code

--- learning AUC.R
+++ learning AUC.R
@@ -54,54 +54,54 @@ ggplot(niche.neutral.auc.df, aes(y = V1,
   geom_point() +
   theme_classic()
 
-# formula from stock
-df$dif <- df$obs - df$inf
-auc <- function(dat, group){
-  pos = dat[dat>0,]
-  neg = dat[dat<0,]
-  tot.pos = sum(pos)
-  tot.neg = sum(neg)
-  m.pos = mean(pos)
-  m.neg = mean(neg)
-  auc = m.pos * m.neg / 
-    (abs(tot.pos) * abs(tot.neg)) 
-  auc
-}
-
-df %>% group_by(threshold) %>% select(dif) %>% summarize(auc(.))
-
-df2 <- df %>% filter(threshold == 0.5)
-auc(df2$dif)
-
-glm_test <- glm(obs ~ threshold, data = df, family = binomial(logit))
-pred <- predict(glm_test, df, type = "response")
-roc(df$obs, pred, plot = T)
-
-# trying to calculate probabilities based on 
-# https://ms.mcmaster.ca/~bolker/emdbook/book.pdf
-source("adj_conf_matrix function.R")
-c.matrix <- adj_conf_matrix(test$observed, test$inferred)
-
-pred.prob <- data.frame(plus = sum(test$inferred) / 
-                          (length(test$inferred)*2),
-                        neg = (length(test$inferred) -
-                                 sum(test$inferred)) / 
-                          (length(test$inferred) * 2),
-                        o = sum(test$observed) / 
-                          (length(test$inferred) * 2),
-                        u = (length(test$observed) - 
-                               sum(test$observed)) / 
-                          (length(test$inferred) *2))
-
-tp.prob <- (pred.prob$plus * pred.prob$plus *
-              pred.prob$o) / (pred.prob$plus *
-                                pred.prob$o)
-tn.prob <- (pred.prob$neg * pred.prob$neg *
-              pred.prob$u) / (pred.prob$neg *
-                                pred.prob$u)
-fp.prob <- (pred.prob$neg * pred.prob$neg *
-              pred.prob$o) / (pred.prob$neg *
-                                pred.prob$o)
-fn.prob <- (pred.prob$plus * pred.prob$plus *
-              pred.prob$u) / (pred.prob$plus *
-                                pred.prob$u)
+# # formula from stock
+# df$dif <- df$obs - df$inf
+# auc <- function(dat, group){
+#   pos = dat[dat>0,]
+#   neg = dat[dat<0,]
+#   tot.pos = sum(pos)
+#   tot.neg = sum(neg)
+#   m.pos = mean(pos)
+#   m.neg = mean(neg)
+#   auc = m.pos * m.neg / 
+#     (abs(tot.pos) * abs(tot.neg)) 
+#   auc
+# }
+# 
+# df %>% group_by(threshold) %>% select(dif) %>% summarize(auc(.))
+# 
+# df2 <- df %>% filter(threshold == 0.5)
+# auc(df2$dif)
+# 
+# glm_test <- glm(obs ~ threshold, data = df, family = binomial(logit))
+# pred <- predict(glm_test, df, type = "response")
+# roc(df$obs, pred, plot = T)
+# 
+# # trying to calculate probabilities based on 
+# # https://ms.mcmaster.ca/~bolker/emdbook/book.pdf
+# source("adj_conf_matrix function.R")
+# c.matrix <- adj_conf_matrix(test$observed, test$inferred)
+# 
+# pred.prob <- data.frame(plus = sum(test$inferred) / 
+#                           (length(test$inferred)*2),
+#                         neg = (length(test$inferred) -
+#                                  sum(test$inferred)) / 
+#                           (length(test$inferred) * 2),
+#                         o = sum(test$observed) / 
+#                           (length(test$inferred) * 2),
+#                         u = (length(test$observed) - 
+#                                sum(test$observed)) / 
+#                           (length(test$inferred) *2))
+# 
+# tp.prob <- (pred.prob$plus * pred.prob$plus *
+#               pred.prob$o) / (pred.prob$plus *
+#                                 pred.prob$o)
+# tn.prob <- (pred.prob$neg * pred.prob$neg *
+#               pred.prob$u) / (pred.prob$neg *
+#                                 pred.prob$u)
+# fp.prob <- (pred.prob$neg * pred.prob$neg *
+#               pred.prob$o) / (pred.prob$neg *
+#                                 pred.prob$o)
+# fn.prob <- (pred.prob$plus * pred.prob$plus *
+#               pred.prob$u) / (pred.prob$plus *
+#                                 pred.prob$u)
