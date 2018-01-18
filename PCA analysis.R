359b41943826021fe9133d1038009a4463e1e797
Jpomz <jfpomeranz@gmail.com>
2018-01-19 12:27:48 +1300

    5 inferences 1) initial webbuild 2) webbuilder * neutral 3)trait match * neutral 4) webbuilder * trait match 5) webbuilder * trait match * neutral Multivariate distances compared to observed only

--- PCA analysis.R
+++ PCA analysis.R
@@ -7,14 +7,16 @@ library(vegan)
 
 
 obs <- readRDS("observed matrices matched to inferred.RDS")
-# wb == neutral prune + fish correction
-wb <- readRDS("wb for PCA.RDS")
-# tm == neutral, niche prune, fish correction
-tm <- readRDS("tm nn fish for PCA.RDS")
-# wb.tm wb * tm neutral prune fish correction
-wb.tm <- readRDS("wb x tm for PCA.rds")
 # wb.raw == initial WB inference
 wb.raw <- readRDS("wb matrices matched to inferred.RDS")
+# wb == neutral prune + fish correction
+wb.n <- readRDS("wb for PCA.RDS")
+# tm == neutral, niche prune, fish correction
+tm.n <- readRDS("tm nn fish for PCA.RDS")
+# wb x trait match, no neutral
+wb.tm <- readRDS("wb x tm for PCA.RDS")
+# wb.tm wb * tm neutral prune fish correction
+wb.tm.n <- readRDS("wb x tm x n for PCA.rds")
 
 # wb and wb.tm are basically the same
 # maybe compare JUST wb*tm, without correcting fish / pruning neutral
@@ -29,65 +31,63 @@ pc.dat <- ldply(list(
   wb.raw = ldply(wb.raw, function (x){
     Get.web.stats(x, which.stats = 1)
   })[,c(1, 3:7, 12:13)],
-  wb = ldply(wb, function (x){
+  wb.n = ldply(wb.n, function (x){
     Get.web.stats(x, which.stats = 1)
   })[,c(1, 3:7, 12:13)],
-  tm = ldply(tm, function (x){
+  tm.n = ldply(tm.n, function (x){
     Get.web.stats(x, which.stats = 1)
   })[,c(1, 3:7, 12:13)],
   wb.tm = ldply(wb.tm, function (x){
   Get.web.stats(x, which.stats = 1)
-    })[,c(1, 3:7, 12:13)]
+    })[,c(1, 3:7, 12:13)],
+  wb.tm.n = ldply(wb.tm.n, function (x){
+    Get.web.stats(x, which.stats = 1)
+  })[,c(1, 3:7, 12:13)]
   ))
 # add grouping variable
-pc.dat$grp <- as.factor(rep(c("obs","wb.raw","wb", "tm", "wb.tm"), each = 17))
+pc.dat$grp <- as.factor(rep(c("obs","wb.raw","wb.n", "tm.n", "wb.tm", "wb.tm.n"), each = 17))
 pc.dat$land <- rep(c("Pn", "Pn", "T","T","T","Pn","T",
                       "T","T","T","T","Pn", "Pn", "Pn",
-                      "T","T","Pn"), 5)
+                      "T","T","Pn"), 6)
 
 
 pca.obj <- prcomp(pc.dat[,c(2:8)], center = T, scale. = T)
 #adonis.obj <- adonis(pca.obj$x ~ grp*land, data = pc.dat, method='eu')
 
 #biplot(pca.obj)
-ordiplot(pca.obj, xlim = c(-5,5))
-orditorp(pca.obj, display="species", col="red", air=0.01)
+ordiplot(pca.obj)
+orditorp(pca.obj, display="species", col="red", cex = 1.25, air = 0.0005)
 ordihull(pca.obj, groups=pc.dat$grp, draw="polygon", 
-         col=c("grey90","red", "blue", "green", "purple"),
+         col=c("grey90","yellow", "green", "red", 
+               "blue", "plum1"),
          label=F)
-# red == TM, blue == wb, green == wb.raw, purple == wb.tm
+# obs= grey90 
+# tm.n= yellow wb.n= green 
+# wb.raw= red wb.tm = blue wb.tm.n = plum1
 
 # distance
 # surely I could come up with a function that does all of this in one go.... ####
 distance2 <- function(x,y) sum((x-y)^2)
 centroid <- function(x) rowMeans(x)
-obs.center = pca.obj$x[1:17,]
-raw.center = pca.obj$x[18:34,]
-wb.center = pca.obj$x[35:51,]
-tm.center = pca.obj$x[52:68,]
-wb.tm.center = pca.obj$x[69:85,]
+obs.center = centroid(pca.obj$x[1:17,])
+raw.center = centroid(pca.obj$x[18:34,])
+wb.n.center = centroid(pca.obj$x[35:51,])
+tm.n.center = centroid(pca.obj$x[52:68,])
+wb.tm.center = centroid(pca.obj$x[69:85,])
+wb.tm.n.center = centroid(pca.obj$x[86:102,])
 
-obs.wb.dist <- distance2(centroid(obs.center),
-                         centroid(wb.center))
-obs.tm.dist <- distance2(centroid(obs.center),
-                         centroid(tm.center))
-obs.wbtm.dist <- distance2(centroid(obs.center),
-                           centroid(wb.tm.center))
-wb.wbtm.dist <- distance2(centroid(wb.tm.center),
-                        centroid(wb.center))
-wb.tm.dist <- distance2(centroid(tm.center),
-                        centroid(wb.center))
-tm.wbtm.dist <- distance2(centroid(wb.tm.center),
-                          centroid(tm.center))
-raw.obs <- distance2(centroid(raw.center),
-                     centroid(obs.center))
-raw.tm <- distance2(centroid(raw.center),
-                     centroid(tm.center))
-raw.wb <- distance2(centroid(raw.center),
-                     centroid(wb.center))
-raw.wb.tm <- distance2(centroid(raw.center),
-                     centroid(wb.tm.center))
 
+obs.raw <- distance2(raw.center, obs.center)
+obs.wb.n.dist <- distance2(obs.center, wb.n.center)
+obs.tm.n.dist <- distance2(obs.center, tm.n.center)
+obs.wb.tm.dist <- distance2(obs.center, wb.tm.center)
+obs.wb.tm.n <- distance2(obs.center, wb.tm.n.center)
+
+obs.tab <- data.frame(tm.n = obs.tm.n.dist,
+                      wb = obs.raw, 
+                      wb.tm = obs.wb.tm.dist,
+                      wb.n = obs.wb.n.dist,
+                      wb.tm.n = obs.wb.tm.n)
 
 
 dist.tab <- matrix(NA, 3, 4)
