57960c1694783f061497fb97f3f05dc721075cd0
Jpomz <jfpomeranz@gmail.com>
2018-01-25 11:21:50 +1300

    supplementary scripts rank abundance consumption rate scaling

---
+++ Supp_Rank_Ab.R
@@ -0,0 +1,56 @@
+library(plyr)
+library(dplyr)
+library(ggplot2)
+# invertebrate biomass and abundance
+#invert <- readRDS("estimated invert bodymass.RDS")
+
+invert <- readRDS("ab, dw, info for sites, subset to match inference.RDS")
+invert <- ldply(invert)
+
+invert %>% group_by(site) %>%
+  mutate(tot = sum(no.m2),
+         a = no.m2 / tot) %>%
+  group_by(taxa) %>%
+  summarize(ma = mean(a), sd(a)) %>%
+  arrange(desc(ma)) %>%
+  View
+  
+  
+invert %>% group_by(site) %>%
+  mutate(tot = sum(no.m2),
+         a = no.m2 / tot) %>%
+  ggplot(aes(x = taxa, y = a))+
+  geom_boxplot()
+
+
+invert %>% group_by(site) %>%
+  mutate(tot = sum(no.m2),
+         a = no.m2 / tot) %>%
+  group_by(taxa) %>%
+  summarize(ma = max(a), sd = sd(a)) %>%
+  ggplot(aes(x = ma, y = (sd))) +
+  geom_point() +
+  stat_smooth()
+
+
+invert %>% group_by(site) %>%
+  mutate(tot = sum(no.m2),
+         a = no.m2 / tot) %>%
+  group_by(taxa) %>%
+  summarize(ma = max(a), sd = sd(a)) %>%
+  mutate(m.s = ma / sd) %>%
+  ggplot(aes(ma, m.s)) +
+  geom_point()
+
+
+invert %>% group_by(site) %>%
+  mutate(tot = sum(no.m2),
+         a = no.m2 / tot) %>%
+  mutate(rank = rank(-no.m2, ties.method = "first")) %>% #View
+  group_by(taxa) %>%
+  summarize(n(), mean = mean(rank), min = min(rank), max = max(rank)) %>%
+  arrange(mean) %>% View
+
+
+invert[order(desc(rank(invert$no.m2, ties = "first"))),]
+invert[312,]
